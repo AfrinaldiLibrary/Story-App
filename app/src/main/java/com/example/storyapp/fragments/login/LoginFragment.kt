4307b61dc32acd298f3591d7222e4d02453ea24b
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2022-04-16 16:06:23 +0700

    add custom button

--- app/src/main/java/com/example/storyapp/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/storyapp/fragments/login/LoginFragment.kt
@@ -1,6 +1,9 @@
 package com.example.storyapp.fragments.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +29,47 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setButton()
+        inputListener()
         loginCheck()
+    }
+
+    private fun inputListener() {
+        binding.apply {
+            etEmail.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    setButton()
+                }
+
+                override fun afterTextChanged(s: Editable) {
+
+                }
+            })
+
+            etPassword.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    setButton()
+                }
+
+                override fun afterTextChanged(s: Editable) {
+
+                }
+            })
+        }
+    }
+
+    private fun setButton() {
+        binding.apply {
+            btnLogin.isEnabled = etPassword.text!!.length >= 6 && Patterns.EMAIL_ADDRESS.matcher(etEmail.text.toString()).matches()
+        }
     }
 
     private fun loginCheck() {
--- app/src/main/java/com/example/storyapp/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/example/storyapp/fragments/register/RegisterFragment.kt
@@ -1,19 +1,15 @@
 package com.example.storyapp.fragments.register
 
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.storyapp.api.ApiConfig
-import com.example.storyapp.api.Register
 import com.example.storyapp.databinding.FragmentRegisterBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
@@ -32,9 +28,61 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
+        setButton()
+        inputListener()
         registerCheck()
+    }
+
+    private fun inputListener() {
+        binding.apply {
+            etName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    setButton()
+                }
+
+                override fun afterTextChanged(s: Editable) {
+
+                }
+            })
+
+            etEmail.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    setButton()
+                }
+
+                override fun afterTextChanged(s: Editable) {
+
+                }
+            })
+
+            etPassword.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    setButton()
+                }
+
+                override fun afterTextChanged(s: Editable) {
+
+                }
+            })
+        }
+    }
+
+    private fun setButton() {
+        binding.apply {
+            btnRegister.isEnabled = etName.text!!.isNotEmpty() &&etPassword.text!!.length >= 6 && Patterns.EMAIL_ADDRESS.matcher(etEmail.text.toString()).matches()
+        }
     }
 
     private fun registerCheck() {
